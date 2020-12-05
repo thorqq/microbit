029862eb98eb75681166167d7805bf7946b5114e
thorqq <thorqq@163.com>
2020-12-05 10:09:11 +0800

    增加：心跳

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+basic.forever(function () {
+    music.playTone(262, music.beat(BeatFraction.Sixteenth))
+    basic.showIcon(IconNames.Heart)
+    music.playTone(262, music.beat(BeatFraction.Sixteenth))
+    basic.showIcon(IconNames.SmallHeart)
+})
